e8738ddf30de584474e1696447343f125bbce98d
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-28 17:08:45 +0530

    variables datatypes and Scanner in JAVA

---
+++ Variables and Datatypes/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 8;   //256
+        System.out.println(b);
+        char ch = 'a';  // can store only single char
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float price = 10.5f;
+        System.out.println(price);
+        int number = 25;
+        System.out.println(number);
+    }
+}
