71b45798eaae620f8eb181937002b96a0bc5f1a1
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-29 10:16:55 +0530

    Binary Operators

---
+++ Operators/ArithemticOp.java
@@ -0,0 +1,13 @@
+public class ArithemticOp{
+    public static void main(String args[]){
+        // Binary Operators
+        int A = 10;
+        int B = 5;
+        System.out.println("add = " + (A + B));
+        System.out.println("subtract = " + (A - B));
+        System.out.println("multiply = " + (A * B));
+        System.out.println("divide = " + (A / B));
+        System.out.println("Modulo(remainder) = " + (A % B));
+
+    }
+}
