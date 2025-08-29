fb1030a3be4bd73c60bebcff5e9100f8caf14b42
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-29 09:18:49 +0530

    Scanner class in Java

--- Variables and Datatypes/ScannerClass.java
+++ Variables and Datatypes/ScannerClass.java
@@ -1,9 +1,15 @@
 import java.util.*;
 
-public class ScannerClass{
+public class ScannerClass {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        System.out.println(input);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String input = sc.nextLine();
+        // System.out.println(input);
+
+        int number = sc.nextInt();
+        System.out.println(number);
     }
 }
