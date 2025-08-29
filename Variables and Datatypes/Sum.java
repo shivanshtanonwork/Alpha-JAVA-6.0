54b6458cc01bd233c99454cf07f49dfb672b0afc
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-29 09:29:52 +0530

    Practice of scanner class

--- Variables and Datatypes/Sum.java
+++ Variables and Datatypes/Sum.java
@@ -1,10 +1,20 @@
-public class Sum {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
+// public class Sum {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 20;
+//         int sum = a + b;
+//         System.out.println(sum);
+//     }
+// }
+
+import java.util.*;
+
+public class Sum{
+    public static void main(String  args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int sum = a + b;
         System.out.println(sum);
     }
 }
-
- 
