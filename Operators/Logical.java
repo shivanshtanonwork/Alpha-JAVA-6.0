71f4da25c3583c4640ccd842c85e6babb72786a0
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-29 13:23:48 +0530

    Logical NOT

---
+++ Operators/Logical.java
@@ -0,0 +1,15 @@
+public class Logical {
+    public static void main(String args[]){
+        // logical And operator
+        System.out.println((3>2) && (5>0) );
+        System.out.println((3<2) && (5>1) );
+
+        // Logical OR operator
+        System.out.println((3>2) || (5>4));
+        System.out.println((3<2) || (5>4));
+
+        // Logical NOT
+        System.out.println(!(0>5));
+        System.out.println(!(2<8));
+    }    
+}
