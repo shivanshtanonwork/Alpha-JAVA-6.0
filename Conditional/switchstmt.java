624463c2907d9b67005b6c0a8cf1d256430f8c6c
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-31 19:03:05 +0530

    Switch statement

---
+++ Conditional/switchstmt.java
@@ -0,0 +1,20 @@
+
+public class switchstmt {
+
+    public static void main(String[] args) {
+        int number = 2;
+        switch (number) {
+            case 1:
+                System.out.println("Samosa");
+                break;
+            case 2:
+                System.out.println("Burger");
+                break;
+            case 3:
+                System.out.println("Mango Shake");
+                break;
+            default:
+                System.out.println("We realise we are dreaming");
+        }
+    }
+}
