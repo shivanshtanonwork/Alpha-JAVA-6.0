5dad863e8251ab85d76f7f015206c80f7d497b61
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-31 17:57:48 +0530

    ternary

---
+++ Conditional/passorfail.java
@@ -0,0 +1,10 @@
+
+public class passorfail {
+
+    public static void main(String[] args) {
+        int marks = 67;
+
+        String reportCard = marks >= 33 ? "PAss" : "FAil";
+        System.out.println(reportCard);
+    }
+}
