82c03c5034221275506f8fcf42ce69fc3617cc08
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-02 12:58:43 +0530

    Prime or Not

---
+++ Loops/PrimeOrNot.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class PrimeOrNot {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 2) {
+            System.out.println("Prime");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(n) - 1; i++) {
+                if (n % i == 0) {   // n is a multiple of i (i not equal to 1 or n)
+                    isPrime = false;
+                }
+            }
+
+            if (isPrime == true) {
+                System.out.println("n is Prime");
+            } else {
+                System.out.println("n is not prime");
+            }
+        }
+
+    }
+}
