61c669d1014089a518414cd8c274b94b9afa3775
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-03 19:52:54 +0530

    Patterns

---
+++ Patterns/characterpattern.java
@@ -0,0 +1,19 @@
+
+public class characterpattern {
+
+    public static void main(String[] args) {
+        int n = 7;
+        char ch = 'A';
+
+        //outer loop
+        for (int line = 1; line <= n; line++) {
+
+            //inner loop
+            for (int chars = 1; chars <= line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/starpattern.java
@@ -0,0 +1,14 @@
+
+public class starpattern {
+
+    public static void main(String[] args) {
+
+        for (int line = 1; line <= 4; line++) {
+
+            for (int stars = 1; stars <= line; stars++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
