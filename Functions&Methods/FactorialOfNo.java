4e4f9cf22de0c716c4eeab80ae8a71aa0f857e35
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-08 12:36:48 +0530

    isPrime

--- Functions&Methods/FactorialOfNo.java
+++ Functions&Methods/FactorialOfNo.java
@@ -23,15 +23,41 @@ public class FactorialOfNo {
         return a + b;
     }
 
-    public static int sum(int a, int b, int c) {
-        return a + b + c;
+    public static float sum(float a, float b) {
+        return a + b;
+    }
+    // public static int sum(int a, int b, int c) {
+    //     return a + b + c;
+    // }
+
+    // public static boolean isPrime(int n) {
+    //     boolean isPrime = true;
+    //     for (int i = 2; i <= n - 1; i++) {
+    //         if (n % i == 0) {
+    //             isPrime = false;
+    //             break;
+    //         }
+    //     }
+    //     return isPrime;
+    // }
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         // int fact = factorial(4);
         // System.out.println(fact);
         // System.out.println(binCoeff(5, 2));
-        System.out.println(sum(1, 2));
-        System.out.println(sum(5, 5, 5));
+        // System.out.println(sum(1, 2));
+        // System.out.println(sum(4.5f, 5.2f));
+        System.out.println(isPrime(12));
     }
 }
