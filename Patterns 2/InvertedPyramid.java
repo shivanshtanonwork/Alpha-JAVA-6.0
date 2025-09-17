b08f9013c1c9be4d560daab5ac4c830ae78fbc0e
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-17 09:53:12 +0530

    Inverted Pyramid

---
+++ Patterns 2/InvertedPyramid.java
@@ -0,0 +1,25 @@
+
+public class InvertedPyramid {
+
+    public static void rotatedPyramid(int n) {
+        //outer
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        rotatedPyramid(4);
+    }
+}
