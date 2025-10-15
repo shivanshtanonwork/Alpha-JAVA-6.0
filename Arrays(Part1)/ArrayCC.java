97d7cb7b8196bc9aea38d889d38c62b1a8b01c02
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-15 09:45:24 +0530

    Arrays

---
+++ Arrays(Part1)/ArrayCC.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class ArrayCC {
+
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("phy : " + marks[0]);
+        System.out.println("chem : " + marks[1]);
+        System.out.println("math : " + marks[2]);
+
+        marks[2] = 50;
+        System.out.println("math : " + marks[2]);
+
+    }
+
+}
