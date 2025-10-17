b97d4fb5fd7a61b094a61d6beccb7c9d5d296191
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-17 17:35:55 +0530

    Largest number

---
+++ Arrays(Part1)/LargestNumber.java
@@ -0,0 +1,22 @@
+
+public class LargestNumber {
+
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        for(int i = 0; i<numbers.length;i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest value is " + smallest);
+        return largest;
+    }
+    public static void main(String args[]){
+        int numbers[] = {1,3,6,2,4};
+        System.out.println(getLargest(numbers));
+    }
+}
